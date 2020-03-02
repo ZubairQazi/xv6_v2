5426513c64c45042865e1317159c3214182cc8e5
ZubairQazi <zqazi004@ucr.edu>
2020-03-02 00:11:34 +0000

    Fixed typos

--- trap.c
+++ trap.c
@@ -79,7 +79,7 @@ trap(struct trapframe *tf)
     break;
   case T_PGFLT:
 //  if (rcr2() != STACKBASE + (PGSIZE * myproc()->pages))
-    if (!(allocuvm(p->pgidr, PGROUNDDOWN(rcr2()), rcr2()))) {
+    if (!(allocuvm(myproc()->pgdir, PGROUNDDOWN(rcr2()), rcr2()))) {
         cprintf("case PGFLT: failed");
         exit();
     }
